25f1bcc47c838c197da246c5e19adf069ae564d2
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-03-18 00:05:53 +0100

    Add thirdweb packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
 export default MyApp
