3e6bc4dfc13aaf671121419255213b7cd626d821
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-15 14:40:33 -0800

    prepare for anyevm

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider activeChain={activeChain}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
